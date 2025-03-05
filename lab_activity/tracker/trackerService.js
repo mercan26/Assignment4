90ac1c0c4734bbee384c0a74af6fb41b39464a7f
mercan26 <mercan26@students.rowan.edu>
2025-03-05 17:16:26 -0500

    Update trackerService.js

--- lab_activity/tracker/trackerService.js
+++ lab_activity/tracker/trackerService.js
@@ -1,85 +1,39 @@
-var events = require("events");
-var Exercise = require("Exercise");
+function TrackerService() {
+  this.details = {
+    exercise: null,
+    weight: null,
+    distance: null,
+    time: null,
+    speed: 0,
+    calories: 0
+  };
 
-/*trackerService.js
-
-Improved Tracker Functionality.
-In this lab, I've added the ability for the tracker to emit an event when
-a new exercise is set.  In a fully-featured application we could respond to
-this event in a variety of ways. For now we are just going to re-
-
-Activities:  Walk/Run/Swim
-Input:  Exercise type
-        Distance (miles)
-        Weight (lbs)
-        Time (minutes)
-Output: Calories Burned
-
- Formula Sources:
-
- https://lovandy.com/wellness/physical/calories-burned-by-walking.html AND
- https://lovandy.com/wellness/physical/calories-burned-running.html
-*/
-
-//requires weight in lbs
-//Time in minutes
-//distance in miles
-//This is yet another way to define the tracker Class.  
-var tracker = function(exercise, weight, distance, time) {
-    try{
-
-	this.exercise=new Exercise(exercise);
+  this.events = {};
 	
-	this.weight = Number(weight);
-	this.distance = Number(distance);
-	this.time = Number(time);
-	events.EventEmitter.call(this);
-    } catch (err){
-	console.log("Error recieved during service creation");
-	throw err;
+  this.on = (event, callback) => {
+    if (!this.events[event]) this.events[event] = [];
+    this.events[event].push(callback);
+  };
+
+  this.emit = (event) => {
+    if (this.events[event]) {
+      this.events[event].forEach(callback => callback());
     }
+  };
 
-    //updated to accomodate swimming
-    this.calculate = function() {
-	return this.exercise.calculate(this.weight, this.distance, this.time);
-    };
-    
-    //speed is consistently calculated for all exercise times (distance/time)
-    this.calcSpeed = function(){
-	return this.distance/(this.time/60);//miles per hour
-    };
+  this.set = (property, value) => {
+    this.details[property] = value;
+    this.emit(`${property}Changed`);
+  };
 
-    this.setExercise = function(exercise){
-	this.exercise=new Exercise(exercise);
-	this.emit('exerciseChanged');
+  this.recalculate = (type) => {
+    if (type === 'speed') {
+      this.details.speed = this.details.distance / this.details.time;
+    } else if (type === 'calories') {
+      this.details.calories = this.details.weight * 10; // Just an example formula
+    }
+  };
+}
 
-    };
-
-    this.setWeight =function(weight){
-	this.weight=weight;
-	this.emit('weightChanged');
-    };
-    this.setTime = function(time){
-	this.time=time;
-	this.emit('timeChanged', time);
-    };
-    this.setDistance = function(distance){
-	this.distance=distance;
-	this.emit('distanceChanged', distance);
-    };  
-};
-
-//this is an old way of "inheriting" from event emitter
-//we're basically adding a pointer that points to the
-//parent class prototype.  This is a rather old and esoteric
-//way of doing things but you WILL see it out in the "wild"
-//There's even yet one more way to inherit using the 'util'
-//module and util.inherits method.  We won't cover that because
-//it was transitional (ES6 Classes are recommended now and prototyping is
-//what was used in the majority of old libraries you'll see)
-tracker.prototype.__proto__=events.EventEmitter.prototype;
-
-module.exports = tracker;
-
-//var test = new tracker("Walking", 200, 4, 45);
-//console.log(test.calculate());
+const trackerService = new TrackerService();
+module.exports = trackerService;
